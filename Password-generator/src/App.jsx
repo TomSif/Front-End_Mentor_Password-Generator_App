303168bc047feb794456fbe60af1cdad01065701
TomSif <thomas.sifferle.programmer@gmail.com>
2026-02-25 12:03:48 +0100

    chore: configure tailwind theme, font-face and eslint

--- Password-generator/src/App.jsx
+++ Password-generator/src/App.jsx
@@ -1,15 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-green-500">
-        Password Generator P4$5W0rD! Character Length Include Uppercase Letters
-        Include Lowercase Letters Include Numbers Include Symbols Strength
-        Generate
-      </h1>
-    </>
-  );
+  return <main></main>;
 }
 
 export default App;
